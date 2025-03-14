d2eaa9c8a153e73b237ea229448dc7bb241adcf7
preransm <120048486+preransm@users.noreply.github.com>
2025-03-14 10:17:30 +0530

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,6 @@
 
 class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Try programiz.pro");
+        System.out.println("Hello world from remote repo");
     }
 }
